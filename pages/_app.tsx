6244bb46b61d642d8f3c43c6607c6e54df7ae2d7
David Křístek <david.kristek05@gmail.com>
2022-03-18 19:21:24 +0100

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
+// https://github.com/mariesta/nextjs-auth-with-firebase/blob/main/pages/sign_up.jsx
+// https://github.com/LogicismX/fullstack-chatapp
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+     </Layout>
+  );
 }
 
-export default MyApp
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      blackgreen: "#031201",
+      darkgreen: "#092006",
+      green: "#50B661",
+    },
+  },
+  plugins: [],
+};
